8c83926f665268f7d63bc06a1ef0f4887e50065a
hoomili <hasadian@hotmail.com>
2023-05-31 10:56:24 -0700

    made the players class

---
+++ players.rb
@@ -0,0 +1,12 @@
+class Players
+  PLAYERS_LIVES = 3
+  attr_reader :lives, :name
+  def initialize(name)
+    @name = name
+    @lives = PLAYERS_LIVES
+    puts "#{name} has join the game"
+  end
+  def lost
+    lives -= 1
+  end
+end
